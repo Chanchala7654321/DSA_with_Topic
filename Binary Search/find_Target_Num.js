e2cff11f6630cff9befaafdf8a53fdc9215f665d
Chanchala7654321 <chanchalasidar24@navgurukul.org>
2026-05-14 19:26:33 +0530

    Target value find

---
+++ Binary Search/find_Target_Num.js
@@ -0,0 +1,30 @@
+
+
+function binarySearch(arr, target) {
+    let low = 0;
+    let high = arr.length - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] === target) {
+            let ans = `Index: ${mid} Value ${arr[mid]}`
+            return ans;
+        }
+        else if (arr[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+
+    }
+
+}
+let arr = [2, 4, 6, 8, 10];
+let target = 8;
+console.log(binarySearch(arr, target));
+
+// binarySearch(arr, target)
+
+
+
